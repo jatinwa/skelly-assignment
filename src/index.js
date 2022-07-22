8e7a9ac6779cab68d67dbd941244805e7bcefa35
jatin <jatinwatts8@gmail.com>
2022-07-22 21:28:00 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId='114334397583-a7o91gul94sjcputmk2v59056q4dfmhr.apps.googleusercontent.com'>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
